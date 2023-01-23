b036103108c921298bb1553b3b88ab2decfa7657
L3GO321 <googlik701@gmail.com>
2023-01-23 19:20:41 +0300

    update commit

--- src/components/DateTimePretty/DateTimePretty.jsx
+++ src/components/DateTimePretty/DateTimePretty.jsx
@@ -1,30 +1,32 @@
 import { DateTime } from "../DateTime/DateTime";
 
-export const DateTimePretty = (props) => {
-  const getData = () => {
-    const date = new Date(props.date).getTime()
-    const current = new Date().getTime()
-    const diff = current - date
+const withPrettyDate = (Component) =>
+  (props) => {
+    const getData = () => {
+      const date = new Date(props.date).getTime()
+      const current = new Date().getTime()
+      const diff = current - date
 
-    if (diff < 1000 * 60) {
-      return 'меньше минуты назад'
+      if (diff < 1000 * 60) {
+        return 'меньше минуты назад'
+      }
+
+      if (diff < 1000 * 60 * 60) {
+        const minutes = Math.floor(diff / (1000 * 60))
+        return `${minutes} минут назад`
+      }
+
+      if (diff < 1000 * 60 * 60 * 24) {
+        const hours = Math.floor(diff / (1000 * 60 * 60))
+        return `${hours} часов назад`
+      }
+
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+      return `${days} дней назад`
     }
 
-    if (diff < 1000 * 60 * 60) {
-      const minutes = Math.floor(diff / (1000 * 60))
-      return `${minutes} минут назад`
-    }
-
-    if (diff < 1000 * 60 * 60 * 24) {
-      const hours = Math.floor(diff / (1000 * 60 * 60))
-      return `${hours} часов назад`
-    }
-
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    return `${days} дней назад`
+    return <Component date={getData()} />
   }
 
-  return (
-    <DateTime date={getData()} />
-  )
-}
+
+export const DateTimePretty = withPrettyDate(DateTime)
